9d399dcfcfa00403b4dc1c69221ad8c87241cf30
Simon Schueller <sschueller@zoominternet.net>
2020-05-12 18:51:22 -0400

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Simon Schueller on 5/12/20.
+//  Copyright © 2020 Simon Schueller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
